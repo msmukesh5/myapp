9cc0b1d672b3423e2dd62bc944707cd1b6fdd4bf
Mukesh Sharma <Mukesh@Tiatross-2-iMac.local>
2016-07-06 16:12:47 +0530

    device implemention complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "user#sig"
-  match ':controller(/:action(/:id))', :via => [:get, :post]
+	devise_for :users, controllers: { sessions: 'users/sessions' }
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	root to: "home#index"
+	match ':controller(/:action(/:id))', :via => [:get, :post]
 
 
 end
