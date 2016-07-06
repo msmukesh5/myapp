94a68421aef06f9710de4075feb35d4386bfb414
Mukesh Sharma <Mukesh@Tiatross-2-iMac.local>
2016-07-06 13:02:32 +0530

    implimenting device

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "user#sig"
+  match ':controller(/:action(/:id))', :via => [:get, :post]
+
+
 end
